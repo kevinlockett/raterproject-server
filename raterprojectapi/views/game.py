b8a4d432da6867db262866a81c1a2695b097336e
Kevin Lockett <chlockett@msn.com>
2022-02-23 09:38:18 -0600

    kll-Added GameView and game_tests

--- raterprojectapi/views/game.py
+++ raterprojectapi/views/game.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from raterprojectapi.models import Game
+from raterprojectapi.models import Game, Player
 
 class GameView(ViewSet):
     """Rater Project games view"""
@@ -17,7 +17,6 @@ class GameView(ViewSet):
             game = Game.objects.get(pk=pk)
             serializer = SingleGameSerializer(game)
             return Response(serializer.data)
-
         except Game.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
         
@@ -27,15 +26,50 @@ class GameView(ViewSet):
         Returns:
             Response -- JSON serialized list of games
         """
-        games = Game.objects.all()   
+        games = Game.objects.all()
+        
+        # The next 3 lines filter games by game type
+        category = request.query_params.get('category', None) #request.query_params is a dictionary of any query params in the url
+        if category is not None:
+            games = games.filter(category=category)
+        
         serializer = MultiGameSerializer(games, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        player = Player.objects.get(user=request.auth.user)
+        serializer = SingleGameSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(player=player)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        game = Game.objects.get(pk=pk)
+        serializer = SingleGameSerializer(game, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        game = Game.objects.get(pk=pk)
+        game.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class SingleGameSerializer(serializers.ModelSerializer):
     """JSON serializer for retrieving, adding, or editing a single game"""
     class Meta:
         model = Game
-        fields = ['id', 'title', 'description', 'designer', 'year_released', 'number_of_players', 'est_time_to_play', 'age_recommendation', 'player']
+        fields = ['title', 'description', 'designer', 'year_released', 'number_of_players', 'est_time_to_play', 'age_recommendation']
         
 class MultiGameSerializer(serializers.ModelSerializer):
     """JSON serializer for games"""
